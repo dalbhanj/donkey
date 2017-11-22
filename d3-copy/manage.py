1570710dbe80bf72f8ae20cc01b95d80eb4015bb
Peter Dalbhanjan <dalbhanj@amazon.com>
2017-11-21 19:07:44 -0500

    changes to manage

--- d3-copy/manage.py
+++ d3-copy/manage.py
@@ -155,11 +155,7 @@ def autodrive(cfg, model_path=None, use_joystick=False):
           inputs=['cam/image_array'],
           outputs=['user/angle', 'user/throttle', 'user/mode', 'recording'],
           threaded=True)
-    
-    #Check for Keyboard interrupts and change keyboard_condition to True
-    keypress = _GetCh()
-    V.add(keypress, inputs=[], 
-                    outputs=['keypress_mode', 'throttle'], threaded=True)
+
 
     #See if we should even run the pilot module. 
     #This is only needed because the part run_condition only accepts boolean
